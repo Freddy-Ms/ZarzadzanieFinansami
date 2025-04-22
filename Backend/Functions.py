6ecd131d844dab5e84b5804604ab2e9abcc397de
Freddy-Ms <maciejskrzypa2705@gmail.com>
2025-04-22 15:21:08 +0200

    Debug

--- Backend/Functions.py
+++ Backend/Functions.py
@@ -2,13 +2,14 @@ from Models import db, HouseholdUser, Household, User
 
 
 def handle_household_ownership_on_delete(user_id):
+    """Handle the transfer of household ownership or deletion of households when a user is deleted."""
     households_created = Household.query.filter_by(created_by=user_id).all()
 
     for household in households_created:
         member_ids = HouseholdUser.query.filter(
                      HouseholdUser.household_id == household.id,
                      HouseholdUser.user_id != user_id).all()
-        member_ids = [uid for (uid,) in member_ids]
+        member_ids = [user.user_id for user in member_ids]
 
         if member_ids:
             new_owner_id = member_ids[0]
