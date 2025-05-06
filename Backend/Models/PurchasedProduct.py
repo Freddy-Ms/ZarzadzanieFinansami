c23d434a4278361ca3a75665b7756469f4a3d3b8
Freddy-Ms <maciejskrzypa2705@gmail.com>
2025-05-06 17:48:08 +0200

    update products

--- Backend/Models/PurchasedProduct.py
+++ Backend/Models/PurchasedProduct.py
@@ -3,8 +3,8 @@ class PurchasedProduct(db.Model):
     __tablename__ = 'purchased_product'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(255), nullable=True)
+    quantity = db.Column(db.Integer, nullable=True)
     unit_id = db.Column(db.Integer, db.ForeignKey('quantity_unit.id'))
     subcategory_id = db.Column(db.Integer, db.ForeignKey('subcategory.id'))
     price = db.Column(db.Numeric(10, 2))
